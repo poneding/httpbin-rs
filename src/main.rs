4d3b7592a6d76d6689fe881e854a98fa33582761
poneding <poneding@gmail.com>
2025-01-04 14:10:14 +0800

    ci: update to trigger github action.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(hello).service(ci_test))
         .bind("0.0.0.0:8080")?
         .run()
         .await
@@ -11,4 +11,9 @@ async fn main() -> std::io::Result<()> {
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/ci-test")]
+async fn ci_test() -> impl Responder {
+    HttpResponse::Ok().body("Update to trigger CI/CD")
 }
