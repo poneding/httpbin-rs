3888d8a822da72f3b86e484704386ef0c8d1b984
poneding <poneding@gmail.com>
2025-04-01 10:10:26 +0800

    feat: update build script to use version from Cargo manifest for Docker image tagging

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMAGE=httpbin-rs
-TAG=latest
+TAG=$(shell cargo read-manifest| jq -r .version)-actix-web
 
 if ! docker buildx inspect rs-builder &> /dev/null; then
   echo "🦀 - Creating builder: rs-builder"
@@ -13,6 +13,10 @@ docker buildx use rs-builder
 
 echo "🦀 - Building docker image..."
 
-docker buildx build . --platform linux/amd64,linux/arm64 -f Dockerfile -t poneding/$IMAGE:$TAG -t registry.cn-hangzhou.aliyuncs.com/pding/$IMAGE:$TAG --push
+docker buildx build . --platform linux/amd64,linux/arm64 
+  -f Dockerfile \
+  -t poneding/$IMAGE:$TAG \
+  -t registry.cn-hangzhou.aliyuncs.com/pding/$IMAGE:$TAG \
+  --push
 
 echo "🦀 - Done!"
